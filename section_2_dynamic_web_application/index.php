ab30c2023056ff4ef0b9c55ba1d80443627fe116
Flávio Araújo <flavioafa@gmail.com>
2023-05-18 20:17:36 -0300

    PHP for Beginners - Seção 2 - Aula Extract a PHP Database Class - Criação da classe Database;

--- section_2_dynamic_web_application/index.php
+++ section_2_dynamic_web_application/index.php
@@ -1,17 +1,10 @@
 <?php
     require 'functions.php';
     // require 'router.php';
+    require 'Database.php';   
 
-    //Database connection
-    $dsn = 'mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4';
-
-    $pdo = new PDO($dsn, 'root','secret');
-
-    $statement = $pdo->prepare('select * from posts');
-
-    $statement->execute();
-
-    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $db = new Database();
+    $posts = $db->query('select * from posts')->fetchAll(PDO::FETCH_ASSOC);
 
     foreach($posts as $post){
         echo '<li>'.$post['name'].'</li>';
