de01956097528264b357ea469da85113a802db16
Flávio Araújo <flavioafa@gmail.com>
2023-05-18 20:03:39 -0300

    PHP for Beginners - Seção 2 - Aula PDO First Steps - Conexão com o banco de dados e listagem de rows na tela;

--- section_2_dynamic_web_application/index.php
+++ section_2_dynamic_web_application/index.php
@@ -1,3 +1,18 @@
 <?php
     require 'functions.php';
-    require 'router.php';
+    // require 'router.php';
+
+    //Database connection
+    $dsn = 'mysql:host=127.0.0.1;port=3306;dbname=myapp;charset=utf8mb4';
+
+    $pdo = new PDO($dsn, 'root','secret');
+
+    $statement = $pdo->prepare('select * from posts');
+
+    $statement->execute();
+
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach($posts as $post){
+        echo '<li>'.$post['name'].'</li>';
+    }
