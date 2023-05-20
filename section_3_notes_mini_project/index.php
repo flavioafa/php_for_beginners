c58d744ef7fad885d2e61b7b362ce42615459fe7
Flávio Araújo <flavioafa@gmail.com>
2023-05-20 10:06:54 -0300

    PHP for Beginners - Seção 3 - Aula Render the Notes and Note Page - Cria;áo da p[agina que lista notas e que lista uma nota;

--- section_3_notes_mini_project/index.php
+++ section_3_notes_mini_project/index.php
@@ -1,19 +1,4 @@
 <?php
     require 'functions.php';
-    // require 'router.php';
     require 'Database.php';       
-
-
-    $config = require 'config.php';
-
-    $db = new Database($config['database']);
-
-    $id = $_GET['id'];
-
-    //Pode usar ? também
-    $query = 'select * from posts where id = :id';
-
-    //Se usar o parametro nomeado tem que mandar um array associativo
-    $posts = $db->query($query,['id' => $id])->fetch();
-    
-    dd($posts);
+    require 'router.php';
