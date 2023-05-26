43f0c0ab85e50213c08afc30f37b0a62d7910a2e
Flávio Araújo <flavioafa@gmail.com>
2023-05-26 09:58:20 -0300

    PHP for Beginners - Seção 4 - Aula Handle Multiple Request Methods From a Controller Action? - Deletando notas;

--- section_4_project_organization/views/notes/show.view.php
+++ section_4_project_organization/views/notes/show.view.php
@@ -9,6 +9,12 @@
         <p>
             <?= htmlspecialchars($note['body']) ?>
         </p>
+        <form class="mt-6" method="post">
+            <input type="hidden" name="id" value="<?= $note['id'] ?>">
+            <button type="submit" class="text-sm text-red-500">
+                Delete
+            </button>
+        </form>
     </div>
 </main>
 <?php require base_path('views/partials/footer.php') ?>
