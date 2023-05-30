89c2766a38da7a2718891ad61c23ec515fa1f08f
Flávio Araújo <flavioafa@gmail.com>
2023-05-30 20:15:20 -0300

    PHP for Beginners - Seção 5 - Aula Register a New User - Criada as rotas de criar usuário; inicializada uma session com o usuário recem criado; Implementado o criar usuário; indicando que o usuário esta logado no menu/

---
+++ section_5_sessions_and_authentication/controllers/registration/store.php
@@ -0,0 +1,51 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$errors = [];
+
+//Validate data
+if (!Validator::email($email)) {
+    $errors['email'] = 'Please provide a valid email address';
+}
+
+if (!Validator::string($password, 7, 255)) {
+    $errors['password'] = 'Please provide a password between 7 and 255 characters';
+}
+
+//Return if has validation errors
+if (!empty($errors)) {
+    return view('registration/create.view.php', ['errors' => $errors]);
+}
+
+//Check if email already in database
+$db = App::resolve(Database::class);
+
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if ($user) {
+    // User exists, redirect to home page
+    header('location: /');
+    exit();
+} else {
+    // User not exists, create them
+    $db->query('insert into users (email, password) values (:email, :password)',[
+        'email' => $email,
+        'password' => $password
+    ]);
+
+    // Add user to Session
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+    // Then redirect to home page
+    header('location: /');
+    exit();
+}
