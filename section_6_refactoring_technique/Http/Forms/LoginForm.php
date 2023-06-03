7875e06bad034066a4bd0b349c1c5bc300663b12
Flávio Araújo <flavioafa@gmail.com>
2023-06-03 10:40:02 -0300

    PHP for Beginners - Seção 6 - Aula Extract a Form Validation Object - Refatorada a validação de form de login para a classe LoginForm; Movido a pasta controllers para uma nova pasta Http;

---
+++ section_6_refactoring_technique/Http/Forms/LoginForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Http\Forms;
+
+use Core\Validator;
+
+class LoginForm {
+
+    protected $errors = [];
+
+    public function validate(string $email, string $password): bool
+    {
+        $this->errors = [];
+
+        if (!Validator::email($email)) {
+            $this->errors['email'] = 'Please provide a valid email address';
+        }
+        
+        if (!Validator::string($password)) {
+            $this->errors['password'] = 'Please provide a valid password';
+        }
+        
+        return empty($this->errors);
+    }
+
+    public function errors(): array
+    {
+        return $this->errors;
+    }
+}
--- section_6_refactoring_technique/Http/controllers/session/store.php
+++ section_6_refactoring_technique/Http/controllers/session/store.php
@@ -3,26 +3,18 @@
 use Core\App;
 use Core\Database;
 use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
+
+$form = new LoginForm();
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$errors = [];
-
-//Validate data
-if (!Validator::email($email)) {
-    $errors['email'] = 'Please provide a valid email address';
-}
-
-if (!Validator::string($password)) {
-    $errors['password'] = 'Please provide a valid password';
-}
-
-if (!empty($errors)) {
+if (! $form->validate($email, $password)) {
     return view('session/create.view.php', [
-        'errors' => $errors
+        'errors' => $form->errors()
     ]);
 }
 
