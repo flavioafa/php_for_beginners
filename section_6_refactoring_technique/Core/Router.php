72510a7d8f3479366029bfb18f23011357de30d0
Flávio Araújo <flavioafa@gmail.com>
2023-06-03 10:16:17 -0300

    PHP for Beginners - Seção 6 - Aula Extract a Form Validation Object - Inicialização da pasta;

---
+++ section_6_refactoring_technique/Core/Router.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Core;
+
+use Core\Middleware\Middleware;
+
+class Router {
+
+    protected $routes = [];
+
+    protected function add(string $method, string $uri, string $controller)
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => $method,
+            'middleware' => null
+        ];
+
+        return $this;
+    }
+
+    public function get(string $uri, string $controller)
+    {            
+        return $this->add('GET',$uri,$controller);
+    }
+
+    public function post(string $uri, string $controller)
+    {
+        return $this->add('POST',$uri,$controller);
+    }
+
+    public function delete(string $uri, string $controller)
+    {
+        return $this->add('DELETE',$uri,$controller);
+    }
+
+    public function patch(string $uri, string $controller)
+    {
+        return $this->add('PATCH',$uri,$controller);
+    }
+
+    public function put(string $uri, string $controller)
+    {
+        return $this->add('PUT',$uri,$controller);
+    }
+
+    public function only(string $key)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+        return $this;
+    }
+
+    public function route(string $uri, string $method)
+    {
+        foreach($this->routes as $route){            
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+                Middleware::resolve($route['middleware']);
+                return require base_path($route['controller']);
+            }
+        }
+        $this->abort();
+    }
+
+    protected function abort($code = 404)
+    {
+        http_response_code($code);
+        require base_path("views/{$code}.php");
+        die();
+    }
+}
+
---
+++ section_6_refactoring_technique/Core/functions.php
@@ -0,0 +1,61 @@
+<?php
+
+use Core\Response;
+
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();
+}
+
+function authorize(bool $condition, int $status = Response::FORBIDDEN)
+{
+    if (!$condition) {
+        abort($status);
+    }
+}
+
+function base_path($path)
+{
+    return BASE_PATH . $path;
+}
+
+function view($path, $attributes = [])
+{
+    //Extrai um array associativo e disponibiliza em forma de variaveis, que podem ser acessadas no momento da carga de um .php
+    extract($attributes);
+    require base_path('views/' . $path);
+}
+
+function login($user)
+{
+    $_SESSION['user'] = [
+        'email' => $user['email']
+    ];
+
+    session_regenerate_id(true);
+}
+
+function logout()
+{
+    $_SESSION = [];
+    session_destroy();
+
+    $params = session_get_cookie_params();
+
+    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+}
