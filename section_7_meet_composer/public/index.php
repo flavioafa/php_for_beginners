2d5e6eccb5b860adacddaabe1c2fc36183d2c2e4
Flávio Araújo <flavioafa@gmail.com>
2023-06-07 19:26:27 -0300

    PHP for Beginners - Seção 7 - Aula Composer and Free Autoloading - Composer habilitado; Substituido o codigo que estava carregando as classes pelo composer autoload;

--- section_7_meet_composer/public/index.php
+++ section_7_meet_composer/public/index.php
@@ -8,14 +8,16 @@ use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . '/../';
 
+require BASE_PATH . "vendor/autoload.php";
+
 require BASE_PATH . 'Core/functions.php';
 
-//Função que possibilita o carregamento automatico de classes
-spl_autoload_register(function($class)
-{
-    $class = str_replace('\\',DIRECTORY_SEPARATOR,$class);
-    require base_path("$class.php");
-});
+//Função que possibilita o carregamento automatico de classes, foi subatituido pelo composer autoload
+// spl_autoload_register(function($class)
+// {
+//     $class = str_replace('\\',DIRECTORY_SEPARATOR,$class);
+//     require base_path("$class.php");
+// });
 
 require base_path('bootstrap.php');
 
